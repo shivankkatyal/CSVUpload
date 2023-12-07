fe417aef38beccdf429093d87baa65e43c7706ed
shivankkatyal <katyalshivank@gmail.com>
2023-12-07 20:32:30 +0530

    Second commit

---
+++ config/mongoose.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://shivank28011997:Shivank28@cluster0.ozuj3sd.mongodb.net/?retryWrites=true&w=majority');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console,'error connecting to database'));
+
+db.once('open', function(){
+    console.log('Successfully connected to the database');
+});
+
+module.exports = db;
